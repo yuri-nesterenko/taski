59c59e18d543f04c7cda7aa39db98ba8c1bd6565
Yuri-Nesterenko <nesterenko322@mail.ru>
2025-10-18 14:24:17 +0000

    Описание внесенных изменений

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.250.111.55', '127.0.0.1', 'localhost', 'taskinyy.hopto.org']
 
 
 # Application definition
@@ -103,7 +103,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
